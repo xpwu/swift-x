48642ff55e20186d327df09a8798602965b4516e
xpwu <diodexp@foxmail.com>
2024-09-29 23:30:10 +0800

    Update netHost.swift

--- Sources/xpwu_x/netHost.swift
+++ Sources/xpwu_x/netHost.swift
@@ -12,6 +12,13 @@ public extension UInt32 {
     pointer[2] = Byte((self & 0x0000ff00)>>8)
     pointer[3] = Byte(self & 0x000000ff)
   }
+	
+	func toNet(_ pointer: inout Data) {
+		pointer[0] = Byte((self & 0xff000000)>>24)
+		pointer[1] = Byte((self & 0x00ff0000)>>16)
+		pointer[2] = Byte((self & 0x0000ff00)>>8)
+		pointer[3] = Byte(self & 0x000000ff)
+	}
 }
 
 public extension UInt16 {
@@ -19,6 +26,11 @@ public extension UInt16 {
     pointer[0] = Byte((self & 0xff00)>>8)
     pointer[1] = Byte(self & 0x00ff)
   }
+	
+	func toNet(_ pointer: inout Data) {
+		pointer[0] = Byte((self & 0xff00)>>8)
+		pointer[1] = Byte(self & 0x00ff)
+	}
 }
 
 public extension UInt64 {
@@ -32,34 +44,59 @@ public extension UInt64 {
     pointer[6] = Byte((self & 0x000000000000ff00)>>(16-8))
     pointer[7] = Byte((self & 0x00000000000000ff))
   }
+	
+	func toNet(_ pointer: inout Data) {
+		pointer[0] = Byte((self & 0xff00000000000000)>>(64-8))
+		pointer[1] = Byte((self & 0x00ff000000000000)>>(56-8))
+		pointer[2] = Byte((self & 0x0000ff0000000000)>>(48-8))
+		pointer[3] = Byte((self & 0x000000ff00000000)>>(40-8))
+		pointer[4] = Byte((self & 0x00000000ff000000)>>(32-8))
+		pointer[5] = Byte((self & 0x0000000000ff0000)>>(24-8))
+		pointer[6] = Byte((self & 0x000000000000ff00)>>(16-8))
+		pointer[7] = Byte((self & 0x00000000000000ff))
+	}
+}
+
+public extension Data {
+	func net2UInt64()->UInt64 {
+		var ret:UInt64 = 0;
+		for i in 0..<[self.count, 8].min()! {
+			ret = (ret << 8) + UInt64(self[i]&0xff)
+		}
+		
+		return ret;
+	}
+	
+	func net2UInt32()->UInt32 {
+		var ret:UInt32 = 0;
+		for i in 0..<[self.count, 4].min()! {
+			ret = (ret << 8) + UInt32(self[i]&0xff)
+		}
+		
+		return ret;
+	}
+	
+	func net2UInt16()->UInt16 {
+		var ret:UInt16 = 0;
+		for i in 0..<[self.count, 2].min()! {
+			ret = (ret << 8) + UInt16(self[i]&0xff)
+		}
+		
+		return ret;
+	}
 }
 
 public extension Array where Element == Byte {
   func net2UInt64()->UInt64 {
-    var ret:UInt64 = 0;
-    for i in 0..<[self.count, 8].min()! {
-      ret = (ret << 8) + UInt64(self[i]&0xff)
-    }
-    
-    return ret;
+		return Data(self).net2UInt64()
   }
   
   func net2UInt32()->UInt32 {
-    var ret:UInt32 = 0;
-    for i in 0..<[self.count, 4].min()! {
-      ret = (ret << 8) + UInt32(self[i]&0xff)
-    }
-    
-    return ret;
+		return Data(self).net2UInt32()
   }
   
   func net2UInt16()->UInt16 {
-    var ret:UInt16 = 0;
-    for i in 0..<[self.count, 2].min()! {
-      ret = (ret << 8) + UInt16(self[i]&0xff)
-    }
-    
-    return ret;
+		return Data(self).net2UInt16()
   }
 }
 
