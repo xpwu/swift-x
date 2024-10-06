0814067b0af94862a56d79b5a77af45ac3f096dd
xpwu <diodexp@foxmail.com>
2024-10-07 01:21:46 +0800

    add: duration < --- > string

--- Sources/xpwu_x/duration.swift
+++ Sources/xpwu_x/duration.swift
@@ -42,8 +42,79 @@ public extension Duration {
 	}
 }
 
-extension Duration: CustomStringConvertible {
-	var description: String {
-		return "\(self.milliSecond())ms"
+// UInt64 已经实现了 CustomStringConvertible::description
+// 所以无法通过 CustomStringConvertible::description 实现 Duration 的输出
+extension Duration {
+	var toString: String {
+		get {
+			var ret = ""
+			var left = self
+			
+			var v = left.hour()
+			if v != 0 {
+				ret += "\(v)h"
+				left -= v * .Hour
+			}
+			v = left.minute()
+			if v != 0 {
+				ret += "\(v)min"
+				left -= v * .Minute
+			}
+			v = left.second()
+			if v != 0 {
+				ret += "\(v)s"
+				left -= v * .Second
+			}
+			v = left.milliSecond()
+			if v != 0 {
+				ret += "\(v)ms"
+				left -= v * .Millisecond
+			}
+			v = left.microSecond()
+			if v != 0 {
+				ret += "\(v)us"
+			}
+			
+			return ret
+		}
+	}
+	
+	static func from(string: String)-> Duration? {
+		if string.isEmpty {
+			return nil
+		}
+		
+		guard let regex = try? NSRegularExpression(
+			pattern: "^(([1-9][0-9]*)h)?(([1-9][0-9]*)min)?(([1-9][0-9]*)s)?(([1-9][0-9]*)ms)?(([1-9][0-9]*)us)?$"
+			, options: .caseInsensitive) else {
+			return nil
+		}
+		
+		let results = regex.matches(in: string, range: NSRange(string.startIndex..., in: string))
+		
+		if results.isEmpty {
+			return nil
+		}
+		
+		let res = results[0]
+		if res.numberOfRanges < 2*1 + 1 || res.numberOfRanges > 5*2+1 {
+			return nil
+		}
+		
+		let units = [Duration.Hour, Duration.Minute, Duration.Second, Duration.Millisecond, Duration.Microsecond]
+		var ret: UInt64 = 0
+		
+		for i in 1 ... (res.numberOfRanges-1)/2 {
+			// (([1-9][0-9]*)h)
+			if res.range(at: i*2-1).location == NSNotFound {
+				continue
+			}
+			// ([1-9][0-9]*)
+			let match = String(string[Range(res.range(at: i*2), in: string)!])
+			
+			ret += UInt64(match)! * units[i-1]
+		}
+		
+		return ret
 	}
 }
